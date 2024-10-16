a1a24e194785474161322caf55b25d0afeb43fe6
i-Blaise <menniablaise@yahoo.com>
2024-10-16 15:26:03 +0000

    careers section 3

--- resources/views/pages/contact/contacts.blade.php
+++ resources/views/pages/contact/contacts.blade.php
@@ -234,12 +234,12 @@
                             <div class="card-body">
                                 <div class="form-row">
                                     <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 mb-2">
-                                        <label for="exampleFormControlTextarea1">Caption</label>
-                                        <textarea id="shortText" name="caption">{{ $caption }}</textarea>
-                                    </div>
-                                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 mb-2">
                                         <label for="exampleFormControlTextarea1">Body</label>
                                         <textarea id="shortText" name="body">{{ $body }}</textarea>
+                                    </div>
+                                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 mb-2">
+                                        <label for="exampleFormControlTextarea1">Features</label>
+                                        <textarea id="shortText" name="caption">{{ $caption }}</textarea>
                                     </div>
                                 </div>
                             </div>
